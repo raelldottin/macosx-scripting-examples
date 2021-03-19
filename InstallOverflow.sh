7c7167783c5b1ff9873b0782b10e6976ff23cba7
Raell Dottin <raell.dottin@gmail.com>
2021-03-19 15:16:07 -0400

    Added UTC timestamps per Nuno requests

--- InstallOverflow.sh
+++ InstallOverflow.sh
@@ -16,14 +16,22 @@
 # Script Requirements: bash, curl, hdiutil, pkill, rm, cp, chown, xattr, (root priviledges)
 # we can write a routine to make sure the target system has the required utilities before beginning
 
+
 #We can automated the file download using Google Chrome using Puppeteer
 #https://tutorialzine.com/2017/08/automating-google-chrome-with-node-js
 #https://developers.google.com/web/tools/puppeteer
 
+whoami=$(/usr/bin/python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
+
+# Create a log function that handle messages for logger
+echo "$(date -u): $whoami is currently logged in"
 
 # Do we re-attempt file download if the download fails or give up one try and declare failure?
+
+echo "$(date -u): downloading file to /tmp/download.dmg"
 curl -s 'https://app-updates.overflow.io/packages/updates/osx_64/01800abc7f858990a2f4267e811d430c48f9469b/Overflow-1.16.2.dmg' -o /tmp/download.dmg
 
+echo "$(date -u) attaching downloaded file"
 # How do when know where the dmg file is attached, will the volume always have the same name?
 hdiutil attach /tmp/download.dmg --nobrowse --quiet
 
@@ -33,19 +41,25 @@ hdiutil attach /tmp/download.dmg --nobrowse --quiet
 # Let's check if the application is still running before terminatign it
 # If we are performing a fresh install are user preferences or licensing preferences stored in the application file?
 # Let's confirm that the application actually quits
+echo "$(date -u): closing application "
 pkill "Overflow"
 
 # Check if the app is already install, delete the previous version prior to installation
+echo "$(date -u): removing application "
 rm -fr /Applications/Overflow.app
 
 # Copy the new app version to the Application folder
+echo "$(date -u): copying application"
 cp -R /Volumes/Overflow/Overflow.app /Applications/
 
 # Detach the Volume
+echo "$(date -u): detaching downloaded file"
 hdiutil detach /Volumes/Overflow
 
-# Change permissions on the app bundle
-chown -R root:wheel /Applications/Overflow.app
+# Change permissions on the app bundle -- Note, the installation still need root access to install the app into the application folder.
+echo "$(date -u): providing $whoami with access to /Applications/Overflow.app"
+chown -R $whoami:staff /Applications/Overflow.app
 
 # Remove quarantine flag
+echo "$(date -u): removing quarantine flg on Overflow.app if its set"
 xattr -r -d com.apple.quarantine /Applications/Overflow.app
