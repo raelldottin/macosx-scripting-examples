cb4d79bf247476e4c78adcb7de5bd1e1ee357788
Raell Dottin <raell.dottin@gmail.com>
2021-03-19 11:33:22 -0400

    Remove company specific information.

--- SetComputerName.sh
+++ SetComputerName.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+JAMF_URL=""
+# ***Please use $(printf "username:password" | iconv -t ISO-8859-1 | base64 -i -) to generate your credentials***
+JAMF_CREDENTIALS=""
+# ***Please add computer name prefix***
+JAMF_COMPNAME_PREFIX=""
+
 # retrieve all computer names from Jamf server
 echo "Retrieve computers names from Jamf Server."
-curl -s -X GET https://centuryservices.jamfcloud.com/JSSResource/computers --header 'authorization: Basic PASSWORD_SANITIZED'|grep -Eo '[^CENTOS]\d{3}'|grep X|sed -e 's/X//'|sort > /tmp/computernames.txt
+
+# ***I would like to change the regex from a single quote to double quotes, then use a variable for the company name prefix.***
+curl -s -X GET $JAMF_URL/JSSResource/computers --header 'authorization: Basic $JAMF_CREDENTIALS'|grep -Eo '[^CENTOS]\d{3}'|grep X|sed -e 's/X//'|sort > /tmp/computernames.txt
 
 # generate a list of usable computer names
 echo "Generating a list of usable computer names."
 seq -f '%03g' $(tail -1 <(sort /tmp/computernames.txt)) > /tmp/availablecomputernames.txt
 
+# ***Also, I will like to add the computer name prefixs here too***
 # get the next usable computer name
 echo "Getting the next usable computer name."
 name=CENTOSX$(join -a 1 -o "1.1 2.1" -e missed  /tmp/availablecomputernames.txt /tmp/computernames.txt |grep missed|head -1| awk '{print $1}')
